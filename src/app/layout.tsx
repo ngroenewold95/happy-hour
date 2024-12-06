2e31f354b3a0570696793f2966c7d43ec34eb920
ngroenewold95 <ngroenewold95@gmail.com>
2024-12-06 15:56:47 -0800

    Create Location Functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,9 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const inter = Inter({
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
@@ -25,10 +18,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={inter.className}>
+        <div className="container mx-auto px-12">{children}</div>
       </body>
     </html>
   );
---
+++ src/app/locations/new/page.tsx
@@ -0,0 +1,49 @@
+import { db } from "@/db";
+import { redirect } from "next/navigation";
+
+export default function LocationCreatePage() {
+  async function createLocation(formData: FormData) {
+    "use server";
+
+    const title = formData.get("title") as string;
+    const content = formData.get("content") as string;
+
+    const location = await db.location.create({
+      data: { title, content },
+    });
+    console.log("Created location:", location);
+
+    redirect("/");
+  }
+
+  return (
+    <form action={createLocation}>
+      <h3 className="font-bold m-3">Create a Location</h3>
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="title">
+            Location Name
+          </label>
+          <input
+            name="title"
+            className="border rounded p-2 w-full"
+            id="title"
+          ></input>
+        </div>
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="content">
+            Content
+          </label>
+          <textarea
+            name="content"
+            className="border rounded p-2 w-full"
+            id="content"
+          ></textarea>
+        </div>
+        <button type="submit" className="bg-blue-200 p2-2 rounded">
+          Create
+        </button>
+      </div>
+    </form>
+  );
+}
