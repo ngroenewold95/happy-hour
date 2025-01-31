3c17285f27408508f8777508059d82dcbccbb073
ngroenewold95 <ngroenewold95@gmail.com>
2025-01-30 16:39:14 -0800

    Header for Authentication and some old file clean
    Fix unused imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import Providers from "@/app/providers";
+import Header from "@/components/header";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -20,9 +21,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>
-          <div className="container mx-auto px-12">{children}</div>
-        </Providers>
+        <div className="container mx-auto px-4 max-w-6xl">
+          <Providers>
+            <Header />
+            <div className="container mx-auto px-12">{children}</div>
+          </Providers>
+        </div>
       </body>
     </html>
   );
